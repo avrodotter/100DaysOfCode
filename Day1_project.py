eb17cc09ba950a7dd07e291a041714577f03daf3
Soumyajit Dutta <122477169+avrodotter@users.noreply.github.com>
2025-09-23 23:08:21 +0530

    Day 1- Here we go

---
+++ Day1_project.py
@@ -0,0 +1,6 @@
+# Band name generator
+
+print("Welcome to Band name generator!")
+city= input("What's the city you gre up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name can be: " + city + " " + pet_name + ".")
---
+++ README.md
@@ -0,0 +1,5 @@
+Hello World
+This is Avro
+
+This is my code repo of 100 Days of Code - The Complete Python Pro Bootcamp
+Started on 23rd September 2025
